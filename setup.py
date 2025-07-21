91f6c09877fc4b7659bdac3f5f8982c5fa82cd82
karan <km14052005@gmail.com>
2025-07-21 18:46:51 +0530

    Project structure and venv setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Karan Mehta",
+    author_email="km14052005@gmail.com",
+    packages=find_packages()
+)
